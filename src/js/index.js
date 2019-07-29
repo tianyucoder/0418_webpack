90a6cae8730dea59077703ab25ec0643db1319cd
tianyu <0318@qq.com>
2019-07-29 16:09:23 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,10 @@
 * */
 
 import {add,sub} from './module1';
-import {mul} from './module2'
+import {mul} from './module2';
 import module3 from './module3';
-import data  from '../json/data.json'
-import '../less/demo.less'
+//import data  from '../json/data.json';
+import '../less/demo.less';
 
 
 console.log(add(9,2));
@@ -16,11 +16,11 @@ console.log(mul(1,2));
 console.log(module3.name,module3.age);
 module3.setName('wade');
 console.log(module3.name,module3.age);
-console.log(data,typeof data)
+console.log(data,typeof data);
 
 setTimeout(() =>{
-  console.log(1)
-},1000)
+  console.log(1);
+},1000);
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -15,22 +15,24 @@
 
 //引入path模块，用于解决路径问题
 const path = require('path'); //path内置的模块，用来设置路径。
+//使用extract-text-webpack-plugin提取css为单独文件
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   //入口（从哪里进入开始解析）
   entry:'./src/js/index.js',
 
-  //出口（最终加工完的代码输出到哪里）
+  //输出（最终加工完的代码输出到哪里）
   output: {// 输出配置
     path: path.resolve(__dirname, 'build'),//输出文件路径配置
-    filename: 'index.js',// 输出文件名
+    filename: './js/index.js',// 输出文件名
   },
 
   //所有的loader都要在如下的对象中注册
   module: {
     rules: [
       //使用less-loader解析less为css
-      {
+      /*{
         test: /\.less$/,
         use: [{
           loader: 'style-loader' // 将css模块加工成一个style节点（样式已经放入该节点）
@@ -39,6 +41,14 @@ module.exports = {
         }, {
           loader: 'less-loader' //编译less为css
         }]
+      },*/
+      //提取css为单独文件
+      {
+        test: /\.less$/, //匹配文件的规则，说明该loader对哪个文件生效
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader","less-loader"]
+        })
       },
       //使用file-loader处理图片(不优秀)
       /*{
@@ -59,18 +69,48 @@ module.exports = {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
-            loader: 'url-loader',       //如果不做图片转base64，可以用file-loader
+            loader: 'url-loader',//如果不做图片转base64，可以用file-loader
             options: {
               limit: 8192,//当图片小于8KB的时候，转base64
               outputPath:'img', //图片最终输出的位置
-              publicPath:'../build/img',//css资源图片路径
+              publicPath:'../img',//css资源图片路径
               name:'[hash:5].[ext]' //修改图片名称
+            }
+          }
+        ]
+      },
+      //使用jshint-loader进行语法检查
+      {
+        test: /.js/,
+        enforce: 'pre',//预先加载好jshint-loader
+        exclude: /node_modules/,//排除node_modules下的所有js文件
+        use: [
+          {
+            loader: `jshint-loader`,
+            options: {
+              //jshint 的错误信息在默认情况下会显示为 warning（警告）类信息
+              //将 emitErrors 参数设置为 true 可使错误显示为 error（错误）类信息
+              emitErrors: true,
+
+              //jshint 默认情况下不会打断webpack编译
+              //如果你想在 jshint 出现错误时，立刻停止编译
+              //请设置 failOnHint 参数为true
+              failOnHint: false,
+              esversion: 6
             }
           }
         ]
       }
 
+
+
+
     ]
-  }
+  },
+
+  //所有插件在如下数组中声明且实例化
+  plugins:[
+    new ExtractTextPlugin("./css/index.css"),
+  ]
 }
 
